1e620922ff40da25fdc99bb4cfec8f70ad88c67c
Romario Ribeiro <romaryjane@hotmail.com>
2021-02-06 10:27:19 -0300

    acrentado a menssagem bom dia

--- src/projetoteste/ProjetoTeste.java
+++ src/projetoteste/ProjetoTeste.java
@@ -6,6 +6,9 @@ public class ProjetoTeste {
 
     public static void main(String[] args) {
         System.out.println("ola mundo");
+        System.out.println("bom dia");
+        System.out.println("boa tarde");
+        
     }
     
 }

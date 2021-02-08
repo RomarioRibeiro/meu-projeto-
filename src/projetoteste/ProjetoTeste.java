a6f9ba8f23c1b2628b57b64b1ae3cd40b96188ff
Romario Ribeiro <romaryjane@hotmail.com>
2021-02-08 17:18:22 -0300

    codigo de repetiçao usando while

--- src/projetoteste/ProjetoTeste.java
+++ src/projetoteste/ProjetoTeste.java
@@ -1,14 +1,30 @@
-
 package projetoteste;
 
+
+import java.util.Scanner;
 
 public class ProjetoTeste {
 
     public static void main(String[] args) {
-        System.out.println("ola mundo");
-        System.out.println("bom dia");
-        System.out.println("boa tarde");
-        
+        Scanner ler = new Scanner(System.in);
+        String sim = "SIM ";
+        System.out.print("JA E CASTRATADO NO SITE :");
+        sim = ler.next();
+        while (sim.equals("SIM") |sim.equals("sim")) {
+            System.out.println("SEJA BEM VINDO SITE DE CADASTRO");
+            ler.nextLine();
+            System.out.print("Informe seu Nome :");
+            String nome = ler.nextLine();
+            System.out.print("infome seu numero de cadastro :");
+            int codigo = ler.nextInt();
+
+            System.out.println("DESEJA CONTINUAR");
+            sim = ler.next();
+            System.out.println(nome +" " +codigo);
+            
+        }
+       
+           ler.close();    
     }
-    
+
 }
